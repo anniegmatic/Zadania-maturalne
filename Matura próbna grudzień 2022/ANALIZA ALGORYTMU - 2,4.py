b73800bbc46661fac71b5c6f31669c8d975b7a53
Anna Wójcik <91779580+anniegmatic@users.noreply.github.com>
2024-03-24 20:43:28 +0100

    Update ANALIZA ALGORYTMU - 2,4.py

--- Matura próbna grudzień 2022/ANALIZA ALGORYTMU - 2,4.py
+++ Matura próbna grudzień 2022/ANALIZA ALGORYTMU - 2,4.py
@@ -15,13 +15,3 @@ for linia in plik:
     rekord = linia.strip().split()
     if pary(rekord) == 1:
         print(linia.strip())
-
-
-'''
-2^0 1 - 1
-2^1 2 - 2 3
-2^2 4 - 4 5 6 7
-2^3 8 - 8 9 10 11 12 13 14 15
-2^4 16 - 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
-2^5 32
-'''
